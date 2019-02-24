ff0c9046e86369707e9bf875196f3decf4688ab9
Rui Rodrigues <rui.coutinho.rodrigues@gmail.com>
2019-02-24 12:19:42 +0000

    End video 28

--- Ruka-Market/Controllers/OrdersController.cs
+++ Ruka-Market/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
-﻿using Ruka_Market.Models;
+﻿using Ruka_Market.Helpers;
+using Ruka_Market.Models;
 using Ruka_Market.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -20,12 +21,79 @@ namespace Ruka_Market.Controllers
             orderView.Customer = new Customer();
             orderView.Products = new List<ProductOrder>();
 
-            var list = db.Customers.ToList();
-            ViewBag.CustomerID = new SelectList(list.OrderBy(c => c.Name).ToList(), "CustomerID", "Name");
+            Session["orderView"] = orderView;
 
-
+            ViewBag.CustomerID = new SelectList(CombosHelper.GetCustomersNames(), "CustomerID", "Name");
 
             return View(orderView);
+        }
+
+        public ActionResult AddProduct()
+        {
+
+            ViewBag.ProductID = new SelectList(CombosHelper.GetProducts(), "ProductID", "Description");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductOrder productOrder)
+        {
+            var orderView = Session["orderView"] as OrderView;
+
+            var ProductID = int.Parse(Request["ProductID"]);
+
+            //Caso não haja produto escolhido
+            if (ProductID == 0)
+            {
+                ViewBag.ProductID = new SelectList(CombosHelper.GetProducts(), "ProductID", "Description");
+                ViewBag.Error = "Deve selecionar um produto";
+
+                return View(productOrder);
+            }
+
+            //Verifica se produto existe
+            var Product = db.Products.Find(ProductID);
+            if (Product == null)
+            {
+                ViewBag.ProductID = new SelectList(CombosHelper.GetProducts(), "ProductID", "Description");
+                ViewBag.Error = "Produto não existe";
+
+                return View(productOrder);
+            }
+
+            productOrder = orderView.Products.Find(p => p.ProductID == ProductID);
+
+            if (productOrder == null)
+            {
+                productOrder = new ProductOrder
+                {
+                    Description = Product.Description,
+                    Price = Product.Price,
+                    ProductID = Product.ProductID,
+                    Quantity = float.Parse(Request["Quantity"])
+                };
+
+                orderView.Products.Add(productOrder);
+            }
+            else
+            {
+                productOrder.Quantity += float.Parse(Request["Quantity"]);
+            }
+
+
+            ViewBag.CustomerID = new SelectList(CombosHelper.GetCustomersNames(), "CustomerID", "Name");
+
+            return View("NewOrder", orderView);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- Ruka-Market/Helpers/CombosHelper.cs
+++ Ruka-Market/Helpers/CombosHelper.cs
@@ -26,6 +26,30 @@ namespace Ruka_Market.Helpers
 
         }
 
+        public static List<Customer> GetCustomersNames()
+        {
+            var Customers = db.Customers.ToList();
+            Customers.Add(new Customer
+            {
+                CustomerID = 0,
+                FirstName = "[Selecione um cliente]"
+            });
+
+            return Customers.OrderBy(c => c.Name).ToList();
+        }
+
+        public static List<Product> GetProducts()
+        {
+            var Products = db.Products.ToList();
+            Products.Add(new Product
+            {
+                ProductID = 0,
+                Description = "[Selecione um produto...]"
+            });
+
+            return Products.OrderBy(p => p.Description).ToList();
+        }
+
 
         public void Dispose()
         {
